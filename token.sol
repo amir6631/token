32c07ada07b45cdfcffe2ddd037b31c5b668ec10
amir6631 <160387407+amir6631@users.noreply.github.com>
2024-02-22 10:39:36 +0000

    Update token.sol
    https://github.com/CEO-Token/token/edit/main/token.sol

--- token.sol
+++ token.sol
@@ -225,7 +225,7 @@ contract PausableToken is StandardToken, Pausable {
   
 }
 
-contract Rokda is PausableToken {
+contract romas is PausableToken {
     string public name;
     string public symbol;
     uint public decimals;
